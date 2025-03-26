d21c4eed84ed677e7992494361d29718e1fce59e
mateusselhorst <mateus_selhorst@estudante.sesisenai.org.br>
2025-03-26 08:53:52 -0300

    UPDATE - questoes14e21e24

--- script.js
+++ script.js
@@ -12,3 +12,33 @@ function verificarSinal(num) {
     }
 }
 verificarSinal(-12);
+
+//14º Questão
+
+function atividade14(){
+
+    console.log("Substituir palavras Azul por Vermelho")
+
+    let texto = prompt('Digite uma frase ou texto com a palavra azul: ')
+
+    console.log(texto.replace(/azul/g, "vermelho"))
+}
+
+//21º Questão
+
+function atividade21(){
+
+    console.log("Soma dos Elementos de um Array");
+
+    let valores = prompt("Digite números separados por vírgula: ").split(",").map(Number);
+
+    console.log("Soma:", valores.reduce((acc, val) => acc+val, 0));
+}
+
+//24º Questão
+
+function atividade24() {
+    console.log("Capitalizar a Primeira Letra");
+    let texto = prompt('Digite uma palavra:')
+    console.log(texto.charAt(0) . toUpperCase() + texto.slice(1));
+}
